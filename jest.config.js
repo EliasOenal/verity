0b850df18f83dc1bd9fa36f75406084fa007fb1d
Elias Oenal <git@eliasoenal.com>
2023-08-03 22:59:59 +0200

    GitHub Actions: run jest tests

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
       '^.+\\.ts$': 'ts-jest',
     },
     testEnvironment: 'node',
-    testTimeout: 7000,
+    testTimeout: 15000,
     runner: 'jest-runner',
     maxWorkers: 8, // Number of parallel processes for running test suites
     modulePathIgnorePatterns: ["<rootDir>/dist/"]
