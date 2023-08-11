2aa8f751491d8e249c72a90305ecbd71b928c11f
Victor Hahn <victor@hahn.mt>
2023-08-11 22:06:29 +0200

    Implement cube persistence for browser clients
    Does not work on NodeJS yet

--- src/cubeStore.ts
+++ src/cubeStore.ts
@@ -1,19 +1,33 @@
 import { Cube } from './cube';
 import { logger } from './logger';
+import { CubePersistence } from "./cubePersistence";
 import { EventEmitter } from 'events';
 import { Buffer } from 'buffer';
 
 export class CubeStore extends EventEmitter {
   storage: Map<string, Buffer>;
   allKeys: Buffer[] | undefined;
+  persistence: CubePersistence = undefined;
 
-  constructor() {
+  constructor(enable_persistence: boolean = true) {
     super();
+    if (enable_persistence) {
+      this.persistence = new CubePersistence();
+
+      // this.persistence.on("ready", this.syncPersistentStorage);
+      this.persistence.on('ready', () => {
+        logger.trace("cubeStore: received ready event from cubePersistence");
+        this.syncPersistentStorage();
+      });
+    }
+
     // Maps can't work with Buffers as keys, they would match references,
     // not values. So we store the hashes as hex strings.
     // Maybe we should use a different data structure for this.
     this.storage = new Map();
     this.allKeys = undefined;
+
+
   }
 
   async addCube(cube: Buffer): Promise<Buffer | undefined>;
@@ -31,6 +45,10 @@ export class CubeStore extends EventEmitter {
         }
         this.storage.set(key.toString('hex'), cube.getBinaryData());
         this.allKeys = undefined;
+        if (this.persistence) {
+          this.persistence.storeRawCubes(
+            new Map([[key.toString('hex'), cube.getBinaryData()]]));
+        }
         this.emit('cubeAdded', key);
         return key;
       } catch (e) {
@@ -67,6 +85,21 @@ export class CubeStore extends EventEmitter {
     }
     this.allKeys = Array.from(this.storage.keys()).map(key => Buffer.from(key, 'hex'));
     return this.allKeys;
+  }
+
+  // This gets called once a persistence object is ready.
+  // We will then proceed to store all of our cubes into it,
+  // and load all cubes from it.
+  // TODO: Move indexedDB specific code into cubePersistence or subclass thereof
+  private syncPersistentStorage() {
+    if (!this.persistence) return;
+    this.persistence.requestRawCubes().onsuccess = (event) => {
+      const retrieved = (event.target as IDBRequest).result;
+      for (const rawcube of retrieved.values()) {
+        this.addCube(Buffer.from(rawcube));
+      }
+    }
+    this.persistence.storeRawCubes(this.storage);
   }
 
 }
