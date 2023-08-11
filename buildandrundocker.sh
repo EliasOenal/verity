f759a6d4046691eabd4c208715f8bbc5f5bdd996
Victor Hahn <victor@hahn.mt>
2023-08-11 02:59:01 +0200

    Docker: Fix build&run script

--- buildandrundocker.sh
+++ buildandrundocker.sh
@@ -3,4 +3,4 @@ port="${1:-1984}"
 docker kill verity$port
 docker rm verity$port
 docker build . -t verity$port
-docker run -d --name verity$port -p "$port":1984 -p "$port":1984/udp verity
+docker run -d --name verity$port -p "$port":1984 -p "$port":1984/udp verity$port
