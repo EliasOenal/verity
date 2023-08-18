37267613c733f0d6b475d83f3e0cd4f579304a5e
Victor Hahn <victor@hahn.mt>
2023-08-18 19:27:49 +0200

    Docker: Fix deployment script

--- deploydockerserver.sh
+++ deploydockerserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 port="${1:-1984}"
 rsync -av . --exclude node_modules/ -e 'ssh -p 10045' verity@verity.hahn.mt:/home/verity/verity/
-ssh verity@docker "cd verity; ./buildandrundocker.sh $port"
+ssh -p 10045 verity@verity.hahn.mt "cd verity; ./buildandrundocker.sh $port"
 
