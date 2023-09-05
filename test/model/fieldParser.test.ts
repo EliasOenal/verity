d9b42e3455a0dcf3c715cc05457c773554aff783
Victor Hahn <victor@hahn.mt>
2023-09-05 20:50:46 +0200

    Identity now actually works on MUCs, still WIP
    Some refactoring and bugfixes along the way.

--- test/model/fieldParser.test.ts
+++ test/model/fieldParser.test.ts
@@ -1,5 +1,5 @@
 import { FieldParser } from "../../src/model/fieldParser";
-import { Relationship, CubeField, CubeFieldType, CubeFields, CubeRelationshipType } from "../../src/model/fields";
+import { Relationship, CubeField, CubeFieldType, CubeFields, CubeRelationshipType, CubeRelationship } from "../../src/model/fields";
 import { NetConstants } from "../../src/model/networkDefinitions";
 import { ZwField, ZwFieldType, ZwFields, ZwRelationshipType, zwFieldDefinition } from '../../src/viewmodel/zwFields';
 
@@ -15,7 +15,7 @@ describe('fieldParser', () => {
     fields.data.push(
       CubeField.RelatesTo(
         new Relationship(CubeRelationshipType.CONTINUED_IN,
-        new Buffer(NetConstants.CUBE_KEY_SIZE).fill(0xDA))
+        Buffer.alloc(NetConstants.CUBE_KEY_SIZE).fill(0xDA))
       )
     );
     fields.data.push(
@@ -34,7 +34,7 @@ describe('fieldParser', () => {
     expect(
       restored.getFirstField(CubeFieldType.PAYLOAD).value.toString('utf-8')).
       toEqual("Mein kleiner grüner Kaktus");
-    const restoredrel = Relationship.fromField(restored.getFirstField(CubeFieldType.RELATES_TO));
+    const restoredrel = CubeRelationship.fromField(restored.getFirstField(CubeFieldType.RELATES_TO));
     expect(restoredrel.type).toEqual(CubeRelationshipType.CONTINUED_IN);
     expect(restoredrel.remoteKey[0]).toEqual(0xDA);
   });
@@ -51,7 +51,7 @@ describe('fieldParser', () => {
     fields.data.push(
       ZwField.RelatesTo(
         new Relationship(ZwRelationshipType.MYPOST,
-        new Buffer(NetConstants.CUBE_KEY_SIZE).fill(0xDA))
+        Buffer.alloc(NetConstants.CUBE_KEY_SIZE).fill(0xDA))
       )
     );
     fields.data.push(
