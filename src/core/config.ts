73e22fd4ba786c782246b97420ea331fd97ba4b2
Victor Hahn <victor@hahn.mt>
2023-09-12 18:50:01 +0200

    NetworkManager: Improve and refactor peer blacklisting
    In particular, we will no longer correct to the same peer multiple times using different addresses

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,17 +1,22 @@
 
 export const Settings = {
-    REQUIRED_DIFFICULTY: 12,
+    // Cube related:
+    REQUIRED_DIFFICULTY: 12,  // hash cash must have this many zero bits at the end
+    HASHCASH_SIZE: 4,  // reverse at least 4 bytes as scratch space for hash cash, giving us 2^32 attempts which is more than enough
+
+    // Network manager related:
     ANNOUNCEMENT_INTERVAL: (25 * 60 * 1000),  // 25 minutes in milliseconds
     NEW_PEER_INTERVAL: (1 * 60 * 1000),  // 1 minutes in milliseconds
     RECONNECT_INTERVAL: (10 * 1000),  // 10 seconds in milliseconds
     RECONNECT_ATTEMPTS: 2,
     MAXIMUM_CONNECTIONS: 20, // Maximum number of connections to maintain
-    HASH_REQUEST_TIME: (10 * 1000),  // 10 seconds in milliseconds
-    NODE_REQUEST_TIME: (60 * 1000),  // asks nodes for their known nodes every minute
-    HASHCASH_SIZE: 4,  // reverse at least 4 bytes as scratch space for hash cash, giving us 2^32 attempts which is more than enough
 
-    // debug/development settings -- should remove these later on
-    HASH_WORKERS: false,
+    // Network peer related:
+    HASH_REQUEST_TIME: (10 * 1000),  // asks nodes for new cube keys every 10 seconds
+    NODE_REQUEST_TIME: (60 * 1000),  // asks nodes for their known nodes every minute
+
+    // local implementation details:
+    HASH_WORKERS: false,  // whether or not to use the threaded hash cash implementation on NodeJS
 }
 
 export class VerityError extends Error {}
--- test/core/peerDB.test.ts
+++ test/core/peerDB.test.ts
@@ -1,6 +1,6 @@
 import { PeerDB, Peer } from '../../src/core/peerDB';
 
-describe('announce', () => {
+describe ('PeerDB', () => {
     it.skip('should get peers from multiple trackers', async () => {
         const peerDB: PeerDB = new PeerDB();
 
@@ -18,9 +18,7 @@ describe('announce', () => {
         // Check if we received any new peers
         expect(newPeers.length).toBeGreaterThan(0);
     }, 10000);
-});
 
-describe('parsePeers', () => {
     it('should correctly parse a buffer of peers', () => {
         const peers = Buffer.from([192, 168, 0, 1, 0x1f, 0x90]); // IP: 192.168.0.1, Port: 8080
         const peers6 = Buffer.alloc(0); // No IPv6 peers
@@ -31,4 +29,29 @@ describe('parsePeers', () => {
             { ip: '192.168.0.1', port: 8080 },
         ]);
     }, 3000);
+
+    it('correctly blacklists peers', () => {
+        const peerDB = new PeerDB();
+        peerDB.setPeersVerified([
+            new Peer("127.0.0.1", 1337),
+            new Peer("127.0.0.1", 1338, "bd806506666ea6ae759878ac1463344e")
+        ]);
+        peerDB.setPeersUnverified([
+            new Peer("127.0.0.1", 1339),
+            new Peer("127.0.0.1", 1340, "a0cffa47a81fe5cf72e3a9ac1d0f2f16")
+        ]);
+        expect(peerDB.getPeersVerified().length).toEqual(2);
+        expect(peerDB.getPeersUnverified().length).toEqual(2);
+        expect(peerDB.getPeersBlacklisted().length).toEqual(0);
+
+        peerDB.setPeersBlacklisted([new Peer("127.0.0.1", 1337)]);
+        expect(peerDB.getPeersVerified().length).toEqual(1);
+        expect(peerDB.getPeersUnverified().length).toEqual(2);
+        expect(peerDB.getPeersBlacklisted().length).toEqual(1);
+
+        peerDB.setPeersBlacklisted([new Peer("1.1.1.1", 40404, "a0cffa47a81fe5cf72e3a9ac1d0f2f16")]);
+        expect(peerDB.getPeersVerified().length).toEqual(1);
+        expect(peerDB.getPeersUnverified().length).toEqual(1);
+        expect(peerDB.getPeersBlacklisted().length).toEqual(2);
+    });
 });
