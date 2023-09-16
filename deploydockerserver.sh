df81cdf816cca9a198581486ebe7c2894635923b
Victor Hahn <victor@hahn.mt>
2023-09-16 15:34:09 +0200

    Docker: Update deploydockerserver.sh

--- deploydockerserver.sh
+++ deploydockerserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 port="${1:-1984}"
-rsync -av . --exclude node_modules/ -e 'ssh -p 10045' verity@verity.hahn.mt:/home/verity/verity/
+rsync -av . --delete --exclude node_modules/ --exclude cubes.db/ --exclude identity.db/ --exclude testidentity.db/ -e 'ssh -p 10045' verity@verity.hahn.mt:/home/verity/verity/
 ssh -p 10045 verity@verity.hahn.mt "cd verity; ./buildandrundocker.sh $port"
 
