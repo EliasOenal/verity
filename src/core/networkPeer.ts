3fa64144d786bf573870e259346d5f0aa340e3de
Victor Hahn <victor@hahn.mt>
2023-10-01 13:47:18 +0200

    Networking: Encapsulate all WebSocket specific code
    in preparation for libp2p/WebRTC support

--- src/core/networkPeer.ts
+++ src/core/networkPeer.ts
@@ -1,16 +1,17 @@
 import { CubeStore } from './cubeStore';
 import { CubeInfo, CubeMeta } from './cubeInfo';
 import { MessageClass, NetConstants } from './networkDefinitions';
-import { Settings } from './config';
+import { Settings, VerityError } from './config';
 import { logger } from './logger';
 import { WebSocketAddress, Peer } from './peerDB';
 import { NetworkManager } from "./networkManager";
 import { isBrowser, isNode, isWebWorker, isJsDom, isDeno } from "browser-or-node";
 import { Buffer } from 'buffer';
 import { EventEmitter } from 'events';
-import { WebSocket } from 'isomorphic-ws';
 import { CubeType } from './cubeDefinitions';
 import { cubeContest } from './cubeUtil';
+
+import WebSocket from 'isomorphic-ws';
 
 export interface PacketStats {
     count: number,
@@ -27,6 +28,112 @@ export interface NetworkStats {
         totalPackets: number,
         totalBytes: number,
         packetTypes: { [key in MessageClass]?: PacketStats }
+    }
+}
+
+/**
+ * @emits "ready" when connection is... you know... ready
+ */
+export abstract class NetworkPeerConnection extends EventEmitter {
+    static Create(peer: NetworkPeer, address: WebSocketAddress) {
+        if (address instanceof WebSocketAddress) {
+            return new WebSocketPeerConnection(peer, address)
+        } else {
+            throw new AddressError("NetworkPeerConnection: Unsupported address type");
+        }
+    }
+
+    close(): void {
+        throw new VerityError("NetworkPeerConnection.close() to be implemented by subclass")
+    }
+    ready(): boolean {
+        throw new VerityError("NetworkPeerConnection.ready() to be implemented by subclass")
+    }
+    send(message: Buffer): void {
+        throw new VerityError("NetworkPeerConnection.send() to be implemented by subclass")
+    }
+
+}
+
+export class WebSocketPeerConnection extends NetworkPeerConnection {
+    private static WEBSOCKET_HANDSHAKE_TIMEOUT = 2500;
+    private ws: WebSocket;  // The WebSocket connection associated with this peer
+
+    // these two represent a very cumbersome but cross-platform way to remove
+    // listeners from web sockets (which we need to do once a peer connection closes)
+    private socketClosedController: AbortController = new AbortController();
+    private socketClosedSignal: AbortSignal = this.socketClosedController.signal;
+
+    constructor(
+            private peer: NetworkPeer,
+            private conn_param: WebSocketAddress | WebSocket) {
+        super();
+
+        if (conn_param instanceof WebSocket) {
+            this.ws = conn_param;
+        } else {
+            // Create a WebSocket connection
+            let WsOptions: any;
+            // set a handshake timeout on NodeJS, not possible in the browser
+            if (isNode) {
+                WsOptions = { handshakeTimeout: WebSocketPeerConnection.WEBSOCKET_HANDSHAKE_TIMEOUT };
+            } else {
+                WsOptions = [];
+            }
+            this.ws = new WebSocket(peer.url, WsOptions);
+        }
+
+        // On WebSocket errors just shut down this peer
+        // @ts-ignore When using socketCloseSignal the compiler mismatches the function overload
+        this.ws.addEventListener("error", (error) => {
+            // TODO: We should probably "greylist" peers that closed with an error,
+            // i.e. not try to reconnect them for some time.
+            logger.warn(`WebSockerPeerConnection: WebSocket error: ${error.message}`);
+            this.peer.close();
+        }, { signal: this.socketClosedSignal });
+
+        // Handle incoming messages
+        // @ts-ignore When using socketCloseSignal the compiler mismatches the function overload
+        this.ws.addEventListener("message", (event) => {
+            if (isNode) {
+                this.peer.handleMessage(Buffer.from(event.data as Buffer));
+            } else {
+                const blob: Blob = event.data as unknown as Blob;
+                blob.arrayBuffer().then((value) => {
+                    this.peer.handleMessage(Buffer.from(value));
+                });
+            }
+        }, { signal: this.socketClosedSignal });
+
+        this.ws.addEventListener('close', () => {
+            // TODO: We should at some point drop nodes closing on us from our PeerDB,
+            // at least if they did that repeatedly and never even sent a valid HELLO.
+            logger.trace(`WebSocketPeerConnection: Peer ${this.peer.toString()} closed on us`);
+            this.peer.close();
+        });
+
+        this.ws.addEventListener("open", () =>  {
+            this.emit("ready");
+        // @ts-ignore I don't know why the compiler complains about this
+        }, { signal: this.socketClosedSignal });
+    }
+
+    /**
+     * Closes the connection.
+     * This will be called by NetworkPeer.close() and should thus never be
+     * called directly.
+     */
+    close(): void {
+        this.ws.close();
+        this.socketClosedController.abort();  // removes all listeners from this.ws
+    }
+
+    ready(): boolean {
+        return (this.ws.readyState > 0)
+    }
+
+    send(message: Buffer) {
+        this.ws.send(message);
     }
 }
 
@@ -59,34 +166,25 @@ export class NetworkPeer extends Peer {
     private unsentCubeMeta: Set<CubeMeta> = undefined;
     private unsentPeers: Peer[] = undefined;  // TODO this should probably be a Set instead
 
+    private conn: NetworkPeerConnection = undefined;
+
     constructor(
             private networkManager: NetworkManager,
             address: WebSocketAddress | WebSocketAddress[],
-            private ws: WebSocket,  // The WebSocket connection associated with this peer
             private cubeStore: CubeStore,  // The cube storage instance associated with this peer
             private hostNodePeerID: Buffer,
             private lightMode: boolean = false,
-
-            // these two represent a very cumbersome but cross-platform way to remove
-            // listeners from web sockets (which we need to do once a peer connection closes)
-            private socketClosedController: AbortController = new AbortController(),
-            private socketClosedSignal: AbortSignal = socketClosedController.signal)
+            conn: NetworkPeerConnection | WebSocket = undefined,
+        )
     {
         super(address);
-        this.networkManager = networkManager;
-        this.ws = ws;
-        this.cubeStore = cubeStore;
-        this.hostNodePeerID = hostNodePeerID;
-        this.lightMode = lightMode;
-
-        // On WebSocket errors just shut down this peer
-        // @ts-ignore When using socketCloseSignal the compiler mismatches the function overload
-        ws.addEventListener("error", (error) => {
-            // TODO: We should probably "greylist" peers that closed with an error,
-            // i.e. not try to reconnect them for some time.
-            logger.warn(`NetworkPeer: WebSocket error: ${error.message}`);
-            this.close();
-        }, { socketClosedSignal });
+        if (conn === undefined) {
+            this.conn = NetworkPeerConnection.Create(this, this.address);
+        } else if (conn instanceof NetworkPeerConnection) {
+            this.conn = conn;
+        } else if (conn instanceof WebSocket) {
+            this.conn = new WebSocketPeerConnection(this, conn);
+        }
 
         // Take note of all cubes I could share with this new peer. While the
         // connection lasts, supplement this with any newly learned cubes.
@@ -108,40 +206,19 @@ export class NetworkPeer extends Peer {
         // TODO FIXME: Most universally, clients can't accept incoming connections on client sockets.
         // TODO FIXME: We should include the server port in the hello message and save it.
 
-        // Handle incoming messages
-        // @ts-ignore When using socketCloseSignal the compiler mismatches the function overload
-        ws.addEventListener("message", (event) => {
-            if (isNode) {
-                this.handleMessage(Buffer.from(event.data as Buffer));
-            } else {
-                const blob: Blob = event.data as unknown as Blob;
-                blob.arrayBuffer().then((value) => {
-                    this.handleMessage(Buffer.from(value));
-                });
-            }
-        }, { signal: socketClosedSignal });
-
-        ws.addEventListener('close', () => {
-            // TODO: We should at some point drop nodes closing on us from our PeerDB,
-            // at least if they did that repeatedly and never even sent a valid HELLO.
-            logger.trace(`NetworkPeer: Peer ${this.toString()} closed on us`);
-            this.close();
-        });
-
         // TODO: We should close the connection if we don't hear from this peer
         // for some defined time. Currently we just keep talking to a wall and
         // occupying a connection slot.
 
         // Send HELLO message once connected
-        if (ws.readyState > 0) {
+        if (this.conn.ready()) {
             logger.info(`NetworkPeer: Connected to ${this.toString()}`);
             this.sendHello();
         } else {
-            ws.addEventListener("open", () =>  {
+            this.conn.on("ready", () => {
                 logger.info(`NetworkPeer: Connected to ${this.toString()}`);
                 this.sendHello();
-            // @ts-ignore I don't know why the compiler complains about this
-            }, { signal: socketClosedSignal });
+            });
         }
     }
 
@@ -153,8 +230,7 @@ export class NetworkPeer extends Peer {
         if (this.nodeRequestTimer) {
             clearInterval(this.nodeRequestTimer);
         }
-        this.ws.close();
-        this.socketClosedController.abort();  // removes all listeners from this.ws
+        this.conn.close();
 
         // If we never got online, "resolve" the promise with undefined.
         // Rejecting it would be the cleaner choice, but then we'd need to catch
@@ -185,7 +261,7 @@ export class NetworkPeer extends Peer {
 
     private txMessage(message: Buffer) {
         this.logTxStats(message, message.readUInt8(NetConstants.PROTOCOL_VERSION_SIZE));
-        this.ws.send(message);
+        this.conn.send(message);
     }
 
     /**
@@ -556,3 +632,6 @@ export class NetworkPeer extends Peer {
         }
     }
 }
+
+export class NetworkError extends VerityError  {}
+export class AddressError extends NetworkError {}
