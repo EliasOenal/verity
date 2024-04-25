9f6c23b8a38d93c6de63af7cec82d5d74c60ee9d
Victor Hahn <victor@hahn.mt>
2024-04-26 00:40:50 +0200

    WebUI: CubeExplorer no longer enforces cciFamily, uses your CubeStore's configured family instead.
    Also add another web helper function.

--- src/webui/helpers.ts
+++ src/webui/helpers.ts
@@ -3,3 +3,16 @@ export function getElementAboveByClassName(elem: HTMLElement, classname: string)
   while (elem && !elem.classList.contains(classname)) elem = elem.parentElement;
   return elem;
 }
+
+export function setAttributeForAll(
+    parent: HTMLElement,
+    selector: string,
+    attrName: string,
+    attrValue: string | undefined,
+): void {
+  const selection = parent.querySelectorAll(selector);
+  for (const elem of selection) {
+    if (attrValue !== undefined) elem.setAttribute(attrName, attrValue);
+    else elem.removeAttribute(attrName);
+  }
+}
