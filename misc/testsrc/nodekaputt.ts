43d58073246a697a81909d71355c8613c8656575
Victor Hahn <victor@hahn.mt>
2024-05-24 18:54:41 +0200

    Remove ad-hoc tests

--- misc/testsrc/nodekaputt.ts
+++
@@ -1,6 +0,0 @@
-import { Cube } from '../../src/core/cube/cube';
-
-console.error("test start");
-let port: any;
-if (port instanceof Cube) console.error("makes no sense");
-console.error("test end");
