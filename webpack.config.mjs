f8d2eea76f08e1a03d80cc8f66f25435cde0701b
Victor Hahn <victor@hahn.mt>
2024-05-31 21:24:36 +0200

    Webpack: Amend config to fix problems when using the base config within application projects

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,26 +1,8 @@
-import { commonConfig } from './webpack.base.mjs'
-import CopyWebpackPlugin from 'copy-webpack-plugin';
+import path from 'path';
+import { frontendConfig } from './webpack.veritycommon.mjs'
 
-export const zwConfig = {
-  ...commonConfig
-};
-zwConfig.plugins.push(
-  new CopyWebpackPlugin({
-    patterns: [
-      { from: './src/webui/static/index.html' },
-      { from: './src/webui/static/style.css' },
-      { from: './src/webui/static/frontend.js' },  // alternatively, we could write that in Typescript and make it a second bundle
-      { from: './src/webui/static/manifest.json' },
-      { from: './src/webui/static/serviceWorker.js' },
-      { from: './img/vera.svg' },
-      { from: './img/unknownuser.svg' },
-      { from: './img/bootstrap.bundle.min.js' },
-      { from: './img/bootstrap.min.css' },
-      { from: './img/bootstrap-icons.min.css' },
-      { from: './img/bootstrap-icons.woff', to: 'fonts/bootstrap-icons.woff' },
-    ]
-  }),
-);
+var dirname = path.dirname(new URL(import.meta.url).pathname);
+const zwConfig = frontendConfig(dirname, dirname);
 zwConfig.entry.verityUI = "./src/app/zw/main.ts";
 
 export default zwConfig;
