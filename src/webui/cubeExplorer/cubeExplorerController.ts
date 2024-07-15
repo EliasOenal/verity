82737ff3b40b3e4c075dce9070ad2cdddbf11065
Victor Hahn <victor@hahn.mt>
2024-07-16 01:02:36 +0200

    CubeExplorer: Bring back date and content filtering
    This reverts commit 437c7db2b28082f77f1a5e0098f1b13033591862.

--- src/webui/cubeExplorer/cubeExplorerController.ts
+++ src/webui/cubeExplorer/cubeExplorerController.ts
@@ -5,6 +5,7 @@ import { getElementAboveByClassName } from "../helpers/dom";
 import { CubeExplorerView } from "./cubeExplorerView";
 import { ControllerContext, VerityController, VerityControllerOptions } from "../verityController";
 import type { CubeKey } from "../../core/cube/cubeDefinitions";
+import { CubeStore } from "../../core/cube/cubeStore";
 
 export interface CubeFilter {
   key?: string,
@@ -21,7 +22,31 @@ export enum EncodingIndex {
   hex = 3,
 }
 
-const DEFAULT_MAX_CUBES = 100;  // TODO move to config
+// TODO move somewhere else
+/**
+ * Abortable CubeStore walk
+ */
+class CubeStoreWalk {
+  private _abort: boolean = false;
+
+  constructor(private cubeStore: CubeStore) {
+  }
+
+  abort() {
+    this._abort = true;
+  }
+
+  async *keys(asString: boolean = false): AsyncGenerator<CubeKey|string> {
+    for await (const key of this.cubeStore.getAllKeys(asString)) {
+      if (this._abort) return;
+      yield key;
+    }
+  }
+
+  // TODO implement: async *binaryCubes(): AsyncGenerator<Buffer>
+}
+
+const DEFAULT_MAX_CUBES = 1000;  // TODO move to config
 
 export interface CubeExplorerControllerOptions extends VerityControllerOptions {
   maxCubes?: number;
@@ -30,13 +55,14 @@ export interface CubeExplorerControllerOptions extends VerityControllerOptions {
 export class CubeExplorerController extends VerityController {
   declare public contentAreaView: CubeExplorerView;
   declare readonly options: CubeExplorerControllerOptions;
+  private storeWalk: CubeStoreWalk;
 
   constructor(
       parent: ControllerContext,
       options: CubeExplorerControllerOptions = {},
   ){
-    super(parent);
-    this.options.maxCubes = DEFAULT_MAX_CUBES;
+    super(parent, options);
+    this.options.maxCubes ??= DEFAULT_MAX_CUBES;
 
     this.contentAreaView = new CubeExplorerView(this);
   }
@@ -58,44 +84,88 @@ export class CubeExplorerController extends VerityController {
    *   If undefined / not supplied, will try to fetch filter setting from the
    *   view. If you explicitly want no filter, set to null.
    */
-  // TODO refined cube search (e.g. by date, fields present or even field content)
   // TODO pagination (we currently just abort after maxCubes and print a warning)
   // TODO sorting (e.g. by date)
-  // TODO support non CCI cubes (including invalid / partial CCI cubes)
   redisplay(filter?: CubeFilter): Promise<void> {
     if (filter === undefined) filter = this.contentAreaView.fetchCubeFilter();
     else if (filter === null) filter = {};
     this.contentAreaView.displayCubeFilter(filter);
 
     this.contentAreaView.clearAll();
-    let initialPromise: Promise<number> = this.cubeStore.getNumberOfStoredCubes();
-    initialPromise.then(async (total: number) => {
-      let displayed = 0, filtered = 0;
-      this.contentAreaView.showBelowCubes(``);
-      for await (const key of this.cubeStore.getAllKeys(true)) {
-        // update stats to reflect progress
-        this.contentAreaView.displayStats(total, displayed, filtered);
-        // Apply key filter
-        if (filter.key !== undefined && !key.includes(filter.key)) {
-          filtered++;
-          continue;  // skip non-matching
-        }
-        displayed++;
-        this.contentAreaView.displayCubeSummary(key as string);
-        if (displayed >= this.options.maxCubes) {
-          this.contentAreaView.showBelowCubes(`Maximum of ${displayed} Cubes displayed, rest omitted. Consider narrower filter.`);
-          break;
-        }
-      }
-      this.contentAreaView.displayStats(total, displayed, filtered);
-    });
-    return initialPromise as unknown as Promise<void>;
+    this.buildCubeList(filter);  // note we are *not* await-ing this to be done
+
+    // view may be shown immediately while we're still processing,
+    // so return a resolved promise
+    return new Promise<void>((resolve) => resolve());
   }
 
-  async loadCubeDetails(key: string): Promise<void> {
+  async buildCubeList(filter?: CubeFilter): Promise<void> {
+    // start a new CubeStore walk; abort the previous one if any
+    if (this.storeWalk) this.storeWalk.abort();
+    this.storeWalk = new CubeStoreWalk(this.cubeStore);
+
+    // prepare some stats
+    let processed = 0, displayed = 0, filtered = 0;
+
+    for await (const key of this.storeWalk.keys(true)) {
+      // update stats to reflect progress
+      processed++;
+      this.contentAreaView.displayStats(processed, displayed, filtered);
+
+      // Apply key filter
+      if (filter.key !== undefined && !key.includes(filter.key)) {
+        filtered++;
+        continue;  // skip non-matching
+      }
+
+      // Apply further filters if specified
+      if (filter.dateFrom !== undefined ||
+          filter.dateTo !== undefined ||
+          filter.content !== undefined) {
+        const cubeInfo = await this.cubeStore.getCubeInfo(key);
+
+        // prepare raw content filter if specified
+        let decodedBinary: string;
+        if (filter.content !== undefined) {
+          decodedBinary = cubeInfo.binaryCube.toString(
+            EncodingIndex[filter.contentEncoding] as BufferEncoding);
+        }
+        // apply filters
+        if ((filter.dateFrom !== undefined && cubeInfo.date < filter.dateFrom) ||
+            (filter.dateTo !== undefined && cubeInfo.date > filter.dateTo) ||
+            (filter.content !== undefined && !decodedBinary.includes(filter.content)
+           )
+        ){
+          filtered++;
+          continue;
+        }
+      }
+
+      // All filters matched, we're gonna display this one
+      displayed++;
+      this.contentAreaView.displayCubeSummary(key as string);
+      if (displayed >= this.options.maxCubes) {
+        this.contentAreaView.showBelowCubes(`Maximum of ${displayed} Cubes displayed, rest omitted. Consider narrower filter.`);
+        break;
+      }
+    }
+    this.contentAreaView.displayStats(processed, displayed, filtered);
+  }
+
+  async toggleCubeDetails(key: string): Promise<void> {
+    // Fetch this Cube and render its details into the view.
+    // Only when the details have been rendered may we toggle the visibility.
+    // TODO: We currently do this on every toggle click which is nonsense, the
+    // details only need to be rendered once and, in particular, need not be
+    // re-rendered on a hide action.
+    await this.renderCubeDetails(key);
+    this.contentAreaView.toggleCubeDetails(key);
+  }
+
+  async renderCubeDetails(key: string): Promise<void> {
     const cube: Cube = await this.getCube(key);
     if (cube === undefined) {
-      this.contentAreaView.showCubeError(key, "Unable to parse cube");
+      this.contentAreaView.makeCubeAlert(key, "danger", "Unable to parse cube");
       return;
     }
     this.contentAreaView.displayCubeDetails(key, cube);
@@ -154,5 +224,15 @@ export class CubeExplorerController extends VerityController {
   async identityChanged(): Promise<boolean> {
     // this controller does not care about user Identites
     return true;
+  }
+
+  close(unshow?: boolean, callback?: boolean): Promise<void> {
+    this.storeWalk.abort();
+    return super.close(unshow, callback);
+  }
+
+  shutdown(unshow?: boolean, callback?: boolean): Promise<void> {
+    this.storeWalk.abort();
+    return super.shutdown(unshow, callback);
   }
 }
