604a87329f90b2004dcd4cfaebdad2a8b6878a78
Victor Hahn <victor@hahn.mt>
2024-09-29 14:39:03 +0200

    Veritum tests: Add encrypt()/decrypt() tests

--- test/cci/veritum/veritum.test.ts
+++ test/cci/veritum/veritum.test.ts
@@ -1,10 +1,13 @@
 import { cciCube, cciFamily } from "../../../src/cci/cube/cciCube";
 import { MediaTypes, cciFieldType } from "../../../src/cci/cube/cciCube.definitions";
 import { cciField } from "../../../src/cci/cube/cciField";
+import { KeyPair } from "../../../src/cci/helpers/cryptography";
 import { Veritum } from "../../../src/cci/veritum/veritum";
 import { coreCubeFamily } from "../../../src/core/cube/cube";
 import { CubeType } from "../../../src/core/cube/cube.definitions";
 import { NetConstants } from "../../../src/core/networking/networkDefinitions";
+
+import sodium from 'libsodium-wrappers-sumo'
 
 const requiredDifficulty = 0;
 
@@ -12,6 +15,24 @@ describe('Veritum', () => {
   const applicationField = cciField.Application("contentum probationis non applicationis");
   const mediaTypeField = cciField.MediaType(MediaTypes.TEXT);
   const payloadField = cciField.Payload("Hoc veritum probatio est");
+
+  let senderKeyPair: KeyPair;
+  let recipientKeyPair: KeyPair;
+
+  beforeAll(async () => {
+
+    await sodium.ready;
+    const uint8senderKeyPair = sodium.crypto_box_keypair();
+    senderKeyPair = {
+      publicKey: Buffer.from(uint8senderKeyPair.publicKey),
+      privateKey: Buffer.from(uint8senderKeyPair.privateKey),
+    };
+    const uint8recipientKeyPair = sodium.crypto_box_keypair();
+    recipientKeyPair = {
+      publicKey: Buffer.from(uint8recipientKeyPair.publicKey),
+      privateKey: Buffer.from(uint8recipientKeyPair.privateKey),
+    };
+  })
 
   describe('cubeType getter', () => {
     it('returns the CubeType set on construction', () => {
@@ -55,7 +76,19 @@ describe('Veritum', () => {
         Array.from(veritum.compiled)[0].getKeyIfAvailable());
     });
 
-    it.todo("returns the first Cube's key for a multi-Cube compiled Veritum");
+    it('returns the first Cube\'s key for a multi-Cube compiled Veritum', async () => {
+      const largePayloadField = cciField.Payload(Buffer.alloc(NetConstants.CUBE_SIZE * 2, 'a'));
+      const veritum = new Veritum(CubeType.FROZEN, {
+        fields: largePayloadField, requiredDifficulty
+      });
+
+      const cubesIterable: Iterable<cciCube> = await veritum.compile();
+      const compiled: cciCube[] = Array.from(cubesIterable);
+
+      expect(compiled.length).toBeGreaterThan(1);
+      const firstCubeKey = await compiled[0].getKey();
+      expect(veritum.getKeyIfAvailable()).toEqual(firstCubeKey);
+    });
 
     it('returns the public key for a MUC Veritum', () => {
       const publicKey = Buffer.alloc(NetConstants.PUBLIC_KEY_SIZE, 0x42);
@@ -66,11 +99,62 @@ describe('Veritum', () => {
   });
 
   describe('encrypt()', () => {
-    it.todo('write tests');
+    it('encrypts fields with default options', () => {
+      const veritum = new Veritum(CubeType.FROZEN, { fields: payloadField });
+      veritum.encrypt(senderKeyPair.privateKey, recipientKeyPair.publicKey);
+      // there must now be an ENCRYPTED field
+      expect(veritum.getFirstField(cciFieldType.ENCRYPTED)).toBeDefined();
+      // there must no longer be a PAYLOAD field (as it's encrypted now)
+      expect(veritum.getFirstField(cciFieldType.PAYLOAD)).toBeUndefined();
+    });
+
+    it('encrypts fields excluding specific fields', () => {
+      const veritum = new Veritum(CubeType.FROZEN, { fields: [applicationField, payloadField] });
+      veritum.encrypt(senderKeyPair.privateKey, recipientKeyPair.publicKey, { exclude: [cciFieldType.APPLICATION] });
+      // expect the APPLICATION field to be kept
+      expect(veritum.getFirstField(cciFieldType.APPLICATION)).toEqual(applicationField);
+      // expect there to be an ENCRYPTED field
+      expect(veritum.getFirstField(cciFieldType.ENCRYPTED)).toBeDefined();
+      // expect encrypted fields not to contain any PAYLOAD field
+      expect(veritum.getFirstField(cciFieldType.PAYLOAD)).toBeUndefined();
+    });
+
+    it('encrypts fields including sender public key', () => {
+      const veritum = new Veritum(CubeType.FROZEN, { fields: payloadField });
+      veritum.encrypt(senderKeyPair.privateKey, recipientKeyPair.publicKey,
+        { includeSenderPubkey: senderKeyPair.publicKey });
+      // expect there to be an ENCRYPTED field
+      expect(veritum.getFirstField(cciFieldType.ENCRYPTED)).toBeDefined();
+      // expect encrypted fields not to contain any PAYLOAD field
+      expect(veritum.getFirstField(cciFieldType.PAYLOAD)).toBeUndefined();
+      // expect there to be a CRYPTO_PUBKEY field
+      expect(veritum.getFirstField(cciFieldType.CRYPTO_PUBKEY)).toBeDefined();
+      expect(veritum.getFirstField(cciFieldType.CRYPTO_PUBKEY).value).toEqual(
+        senderKeyPair.publicKey);
+    });
   });
 
   describe('decrypt()', () => {
-    it.todo('write tests');
+    it('decrypts a single PAYLOAD field', () => {
+      const veritum = new Veritum(CubeType.FROZEN, { fields: payloadField });
+      veritum.encrypt(senderKeyPair.privateKey, recipientKeyPair.publicKey);
+      veritum.decrypt(recipientKeyPair.privateKey, senderKeyPair.publicKey);
+      // there must now be a PAYLOAD field
+      expect(veritum.getFirstField(cciFieldType.PAYLOAD)).toBeDefined();
+      // there must no longer be an ENCRYPTED field (as it's decrypted now)
+      expect(veritum.getFirstField(cciFieldType.ENCRYPTED)).toBeUndefined();
+    });
+
+    it('can use an included public key hint for decryption', () => {
+      const veritum = new Veritum(CubeType.FROZEN, { fields: payloadField });
+      veritum.encrypt(senderKeyPair.privateKey, recipientKeyPair.publicKey,
+        { includeSenderPubkey: senderKeyPair.publicKey });
+      veritum.decrypt(recipientKeyPair.privateKey);
+      // there must now be a PAYLOAD field
+      expect(veritum.getFirstField(cciFieldType.PAYLOAD)).toBeDefined();
+      // there must no longer be an ENCRYPTED field (as it's decrypted now)
+      expect(veritum.getFirstField(cciFieldType.ENCRYPTED)).toBeUndefined();
+    });
   });
 
   describe('compile()', () => {
