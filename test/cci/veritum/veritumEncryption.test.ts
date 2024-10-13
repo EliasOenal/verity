611fe311221b14c9c37185b44c0d2d0caaefbcb5
Victor Hahn <victor@hahn.mt>
2024-10-13 10:49:26 +0200

    Veritum Encryption tests: Add test verifying nonce derivation

--- test/cci/veritum/veritumEncryption.test.ts
+++ test/cci/veritum/veritumEncryption.test.ts
@@ -80,8 +80,8 @@ describe('CCI Veritum encryption', () => {
 
       describe('multiple chunks', () => {
         it.each([
-          ["two-chunk", 1, tooLong],
-          ["three-chunk", 2, evenLonger],
+          ["two-chunk", 2, tooLong],
+          ["three-chunk", 3, evenLonger],
           ["very long", 10, farTooLong],
         ])('encrypts a %s Veritum to a single recipient', async(name, minChunks, payloadString) => {
           const veritum = new Veritum(CubeType.FROZEN, {
@@ -109,6 +109,54 @@ describe('CCI Veritum encryption', () => {
           expect(restored.getFirstField(cciFieldType.ENCRYPTED)).toBeUndefined();
           expect(restored.getFirstField(cciFieldType.PAYLOAD).valueString).toEqual(
             payloadString);
+        });
+
+        it('uses continuation mode for a multi-chunk Veritum ' +
+           'and uses a different nonce in each chunk', async() => {
+          const veritum = new Veritum(CubeType.FROZEN,
+            { fields: cciField.Payload(tooLong) });
+          await veritum.compile({
+            recipients: recipientKeyPair.publicKey,
+            senderPrivateKey: senderKeyPair.privateKey,
+            senderPubkey: senderKeyPair.publicKey,
+            requiredDifficulty,
+          });
+          // just check that the Veritum has compiled as expected
+          expect(veritum.compiled).toHaveLength(2);
+
+          // verify the first chunk decrypts using the ECDH agreed secret and
+          // the nonce included
+          const cryptoBlob: Buffer = veritum.compiled[0].getFirstField(
+            cciFieldType.ENCRYPTED).value;
+          const includedPubkey: Buffer = cryptoBlob.subarray(
+            0, sodium.crypto_box_PUBLICKEYBYTES);
+          const includedNonce: Buffer = cryptoBlob.subarray(
+            sodium.crypto_box_PUBLICKEYBYTES,
+            sodium.crypto_box_PUBLICKEYBYTES + sodium.crypto_box_NONCEBYTES);
+          const ciphertext: Buffer = cryptoBlob.subarray(
+            sodium.crypto_box_PUBLICKEYBYTES + sodium.crypto_box_NONCEBYTES,
+            cryptoBlob.length);
+          const sharedSecret: Uint8Array = sodium.crypto_box_beforenm(
+            includedPubkey, recipientKeyPair.privateKey);
+          const plaintext: Uint8Array = sodium.crypto_secretbox_open_easy(
+            ciphertext, includedNonce, sharedSecret);
+          expect(Buffer.from(plaintext).toString('utf-8')).toContain(
+            tooLong.substring(0, 800));
+
+          // verify that the second chunk decrypts using the same secret and the
+          // hash of the first nonce
+          const ciphertext2: Buffer = veritum.compiled[1].getFirstField(
+            cciFieldType.ENCRYPTED).value;
+          const nonce2: Uint8Array = sodium.crypto_generichash(
+            sodium.crypto_secretbox_NONCEBYTES, includedNonce);
+          const plaintext2: Uint8Array = sodium.crypto_secretbox_open_easy(
+            ciphertext2, nonce2, sharedSecret);
+          expect(Buffer.from(plaintext2).toString('utf-8')).toContain(
+            tooLong.substring(1024, tooLong.length));
+
+          // verify that the second chunk cannot be decrypted using the first nonce
+          expect(() => sodium.crypto_secretbox_open_easy(
+            ciphertext2, includedNonce, sharedSecret)).toThrow();
         });
       });  // single recipient → multiple chunks
     });  // single recipient
@@ -237,8 +285,6 @@ describe('CCI Veritum encryption', () => {
   });
 
   describe('TODOs', () => {
-    it.todo('uses continuation mode for a multi-chunk Veritum and uses a different nonce in each chunk');
-    it.todo('encrypts a single payload field to more recipients that fit in a Cube');
     it.todo('encrypts a multi-chunk Veritum to more recipients that fit in a Cube');
     it.todo('calculates the chunk sizes for different Cube types');
     it.todo('calculates the chunk sizes correctly when including auxialliary non-encrypted data');
