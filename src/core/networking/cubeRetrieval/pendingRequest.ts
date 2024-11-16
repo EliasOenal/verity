dfb604cc9b9b1f278472ada38890b7e2db0a283c
Victor Hahn <victor@hahn.mt>
2024-11-16 12:53:08 +0100

    RequestScheduler: Further generalise PendingRequest to allow storing arbitrary supplemental data
    This will be used at a later stage for request retries and subscription expiry handling.

--- src/core/networking/cubeRetrieval/pendingRequest.ts
+++ src/core/networking/cubeRetrieval/pendingRequest.ts
@@ -7,21 +7,21 @@ import { Settings } from "../../settings";
  * so we can let the application know when their request has been fulfilled
  * (or is not fulfillable).
  **/
-export class PendingRequest<T> {
+export class PendingRequest<Value, SupplementalData> {
   public requestRunning = false;
-  public promise: Promise<T>;
+  public promise: Promise<Value>;
 
   private timeout: NodeJS.Timeout = undefined;
-  private resolve: (value?: T | PromiseLike<T>) => void;
+  private resolve: (value?: Value | PromiseLike<Value>) => void;
 
   /**
-   * @param key Which Cube to request
    * @param timeout A retrieval timeout in milliseconds after which the
    *        retrieval promise will resolve to undefined, or 0 for no timeout.
+   * @param sup Supplemental data to be stored with this request
    */
   constructor(
-    readonly key: Buffer,
     timeout = Settings.CUBE_REQUEST_TIMEOUT,
+    readonly sup?: SupplementalData,
   ) {
     this.promise = new Promise((resolve) => {
       this.resolve = resolve;
@@ -33,7 +33,7 @@ export class PendingRequest<T> {
     }
   }
 
-  fulfilled(data: T): void {
+  fulfilled(data: Value): void {
     clearTimeout(this.timeout);
     this.resolve(data);
   }
