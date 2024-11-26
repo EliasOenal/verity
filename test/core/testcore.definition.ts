86947241bcb2bfe2f3ce34fd9745823563a76029
Victor Hahn <victor@hahn.mt>
2024-11-26 12:26:27 +0100

    Cube request end-to-end tests: Set network timeout to 100ms to fix the failing test
    The reason for this is that due to our crappy 1.0 wire format we're unable to properly parse negative CubeResponses; instead we rely on a network timeout to initiate a request retry.

--- test/core/testcore.definition.ts
+++ test/core/testcore.definition.ts
@@ -9,5 +9,6 @@ export const testCoreOptions: VerityNodeOptions = {
   peerExchange: false,
   initialPeers: [],
   requestInterval: 20, // yes, repeating requests fifty times per second totally is a sensible idea!
+  networkTimeoutMillis: 100,  // all local, no request should take longer than 100ms
 };
 export const requiredDifficulty = 0;
