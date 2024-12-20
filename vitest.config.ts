366218d01fa4911503517cb26dc215db45e83100
Victor Hahn <victor@hahn.mt>
2024-12-20 22:34:09 +0100

    vitest config: Prepare a silent 'reporter'
    enabling us to disable vitest output through the config file, therefore allowing us to follow out own debug prints

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import rawPlugin from 'vite-raw-plugin'
 
+import { DefaultReporter } from 'vitest/reporters';
+
+class silent extends DefaultReporter {
+  onFinished() {
+    // Override this to suppress final output
+  }
+}
+
 export default defineConfig({
   test: {
     include: ['test/**/*.test.ts'],
@@ -9,7 +17,9 @@ export default defineConfig({
       threads: {
         singleThread: true,
       }
-    }
+    },
+    // @ts-ignore it's okay, trust me
+    // reporters: [silent],
   },
   plugins: [
     rawPlugin({
