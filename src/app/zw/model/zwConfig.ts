fa341fb42721316d90a85ba26dcb275d661068d7
Victor Hahn <victor@hahn.mt>
2025-01-31 13:41:25 +0100

    Zw PostView: Disallow anonymous posts by default
    as there is currently no way anybody will ever discover them

--- src/app/zw/model/zwConfig.ts
+++ src/app/zw/model/zwConfig.ts
@@ -1,4 +1,5 @@
 export const ZwConfig = {
   MAXIMUM_POST_LENGTH: 600,
   NOTIFICATION_KEY: "97a64a08415cd1b6b86e4e8c47faf47f9cd5a967294e2b083d04c7bc9519d645",  // fair dice roll, pinky promise!
+  ALLOW_ANONYMOUS_POSTS: false,  // note: even when enabled, we provide no way for users to ever find those anonymous posts
 }
