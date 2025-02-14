3dbf0f57c0b51cb6a64ad7ea5a23a0fc8c87a83f
Victor Hahn <victor@hahn.mt>
2025-02-14 12:12:58 +0100

    Provide high-level API re-exports
    so that client applications don't need to import from src

---
+++ src/webui/index.ts
@@ -0,0 +1,11 @@
+export * from './helpers';
+export * from './navigation';
+
+// Not exporting peer as apps don't need to interact with the PeerController.
+// Not exporting VeraAnimationController either as it's a quick'n'dirty hack.
+
+export * from './identity/identityController';
+export * from './verityController';
+export * from './verityUI';
+export * from './verityView';
+export * from './webUiDefinitions';
