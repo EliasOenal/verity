bf1a1b85d3867d74716f48526a6cb247be48b0b7
Victor Hahn <victor@hahn.mt>
2025-02-21 12:16:41 +0100

    Default settings: Radically reduce default Cube request timeout from 1 minute to 5 seconds
    One minute is basically forever, and it's much too easy using our current API to block the whole application for a Cube request

--- src/core/settings.ts
+++ src/core/settings.ts
@@ -49,7 +49,7 @@ export const Settings = {
     // Request scheduler related:
     KEY_REQUEST_TIME: (10 * 1000),  // run CubeRequests and/or KeyRequests every 10 seconds by default
     NODE_REQUEST_TIME: (10 * 1000),  // asks nodes for their known nodes every 10 seconds (this is still implemented directly in NetworkPeer rather than the RequestScheduler)
-    CUBE_REQUEST_TIMEOUT: (60 * 1000),
+    CUBE_REQUEST_TIMEOUT: (5 * 1000),
     // CUBE_REQUEST_RETRIES: 10,  // TODO implement -- will currently repeat indefinitely until a request times out
     REQUEST_SCALE_FACTOR: 4,  // make requests n times more often at MAXIMUM_CONNECTION compared to on a single connection
     INTERACTIVE_REQUEST_DELAY: 100,  // when a (presumably) interactive application requests a Cube on a light node, auto-schedule the next request in 100ms -- in other words, keep collecting requests for the next 100ms before actually performing the request
