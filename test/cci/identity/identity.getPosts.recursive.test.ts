52ae9208f07866f657a1fa88994b2a7554e29994
Victor Hahn <victor@hahn.mt>
2025-03-09 13:17:27 +0100

    Identity.getPosts(): Fix off-by-one error on recursive post retrievals
    and a big spammy bunch of additional tests, which happened to catch this bug

--- test/cci/identity/identity.getPosts.recursive.test.ts
+++ test/cci/identity/identity.getPosts.recursive.test.ts
@@ -13,79 +13,121 @@ function hasPost(list: Veritable[], post: Veritable): boolean {
 }
 
 describe('Identity: getPosts generator; recursive retrieval of own posts and posts by subscribed authors', () => {
+  // This also tests "subscription mode", i.e. retrieving through an endless AsyncGenerator
 
-  describe('full WOT (i.e. no recursion limits), Veritum output format', () => {
-    let w: TestWorld;
-    let posts: Veritable[];
+  describe('Veritum output format', () => {
+    for (const lvl of [0, 1, 2, 3, 1337]) describe (`recursion level ${lvl}`, () => {
+      let w: TestWorld;
+      let posts: Veritable[];
 
-    beforeAll(async () => {
-      w = new TestWorld({ subscriptions: true, notifications: false });
-      await w.setup();
-      posts = await ArrayFromAsync(w.protagonist.getPosts({
-        format: PostFormat.Veritum,
-        subscriptionRecursionDepth: 1337,  // GO DEEP!
-      }));
-    });
+      beforeAll(async () => {
+        w = new TestWorld({ subscriptions: true, notifications: false });
+        await w.setup();
+        posts = await ArrayFromAsync(w.protagonist.getPosts({
+          format: PostFormat.Veritum,
+          subscriptionRecursionDepth: lvl,
+        }));
+      });
 
-    it('should include my own root posts', () => {
-      expect(hasPost(posts, w.posts[0].own)).toBe(true);
-    });
+      describe('pre-existing posts', () => {
+        it('should include my own root posts', () => {
+          expect(hasPost(posts, w.posts[0].own)).toBe(true);
+        });
 
-    it("should include my direct subscription's posts", () => {
-      expect(hasPost(posts, w.posts[0].directUnreplied)).toBe(true);
-    });
+        if (lvl > 0) it("should include my 1st level subscription's posts", () => {
+          expect(hasPost(posts, w.posts[0].directUnreplied)).toBe(true);
+        });
+        else it("should NOT include my 1st level subscription's posts", () => {
+          expect(hasPost(posts, w.posts[0].directUnreplied)).toBe(false);
+        });
 
-    it("should include indirect subscription's posts", () => {
-      expect(hasPost(posts, w.posts[0].indirectUnreplied)).toBe(true);
-      expect(hasPost(posts, w.posts[0].thirdUnreplied)).toBe(true);
-    });
+        if (lvl > 1) it("should include my 2nd level subscription's posts", () => {
+          expect(hasPost(posts, w.posts[0].indirectUnreplied)).toBe(true);
+        });
+        else it("should NOT include my 2nd level subscription's posts", () => {
+          expect(hasPost(posts, w.posts[0].indirectUnreplied)).toBe(false);
+        });
 
-    it("should include my own replies to direct subscription's posts", async () => {
-      expect(hasPost(posts, w.posts[0].directOwn)).toBe(true);
-    });
+        if (lvl > 2) it("should include my 3rd level subscription's posts", () => {
+          expect(hasPost(posts, w.posts[0].thirdUnreplied)).toBe(true);
+        });
+        else it("should NOT include my 3rd level subscription's posts", () => {
+          expect(hasPost(posts, w.posts[0].thirdUnreplied)).toBe(false);
+        });
 
-    it("should include my own replies to indirect subscription's posts", async () => {
-      expect(hasPost(posts, w.posts[0].indirectOwn)).toBe(true);
-      expect(hasPost(posts, w.posts[0].thirdOwn)).toBe(true);
-    });
+        it("should include my own replies to direct subscription's posts", async () => {
+          expect(hasPost(posts, w.posts[0].directOwn)).toBe(true);
+        });
 
-    it("should include my subscription's replies to my own posts", async () => {
-      expect(hasPost(posts, w.posts[0].ownDirect)).toBe(true);
-      expect(hasPost(posts, w.posts[0].ownIndirect)).toBe(true);
-      expect(hasPost(posts, w.posts[0].ownThird)).toBe(true);
-    });
+        it("should include my own replies to indirect subscription's posts", async () => {
+          expect(hasPost(posts, w.posts[0].indirectOwn)).toBe(true);
+          expect(hasPost(posts, w.posts[0].thirdOwn)).toBe(true);
+        });
 
-    it("should include my subscription's replies to my subscription's posts", async () => {
-      expect(hasPost(posts, w.posts[0].directThird)).toBe(true);
-    });
+        if (lvl > 0) it("should include my 1st level subscription's replies to my own posts", async () => {
+          expect(hasPost(posts, w.posts[0].ownDirect)).toBe(true);
+        });
+        else it("should NOT include my 1st level subscription's replies to my own posts", async () => {
+          expect(hasPost(posts, w.posts[0].ownDirect)).toBe(false);
+        });
 
-    it('should NOT include root posts by non-subscribed users', async () => {
-      expect(hasPost(posts, w.posts[0].unrelatedUnanswered)).toBe(false);
-    });
+        if (lvl > 1) it("should include my 2nd level subscription's replies to my own posts", async () => {
+          expect(hasPost(posts, w.posts[0].ownIndirect)).toBe(true);
+        });
+        else it("should NOT include my 2nd level subscription's replies to my own posts", async () => {
+          expect(hasPost(posts, w.posts[0].ownIndirect)).toBe(false);
+        });
 
-    it('should NOT include replies by non-subscribed users', async () => {
-      expect(hasPost(posts, w.posts[0].ownUnrelatedUnanswered)).toBe(false);
-    });
+        if (lvl > 2) it("should include my 3rd level subscription's replies to my own posts", async () => {
+          expect(hasPost(posts, w.posts[0].ownThird)).toBe(true);
+        });
+        else it("should NOT include my 3rd level subscription's replies to my own posts", async () => {
+          expect(hasPost(posts, w.posts[0].ownThird)).toBe(false);
+        });
 
-    it('should NOT include posts by non-subscribed users even if subscribed users answered them', async () => {
-      expect(hasPost(posts, w.posts[0].ownUnrelatedAnswered)).toBe(false);
-    });
+        if (lvl > 2) it("should include my 3rd level subscription's replies to my subscription's posts", async () => {
+          expect(hasPost(posts, w.posts[0].directThird)).toBe(true);
+        });
+        else it("should NOT include my 3rd level subscription's replies to my subscription's posts", async () => {
+          expect(hasPost(posts, w.posts[0].directThird)).toBe(false);
+        });
 
-    it('should NOT include posts by non-subscribed users even if I answered them', async () => {
-      expect(hasPost(posts, w.posts[0].unrelatedAnsweredByProtagonist)).toBe(false);
-    });
+        it('should NOT include root posts by non-subscribed users', async () => {
+          expect(hasPost(posts, w.posts[0].unrelatedUnanswered)).toBe(false);
+        });
 
-    it("should include my subscription's replies to non-subscribed users", async () => {
-      expect(hasPost(posts, w.posts[0].unrelatedSub)).toBe(true);
-      expect(hasPost(posts, w.posts[0].ownUnrelatedSub)).toBe(true);
-    });
+        it('should NOT include replies by non-subscribed users', async () => {
+          expect(hasPost(posts, w.posts[0].ownUnrelatedUnanswered)).toBe(false);
+        });
 
-    it("should include my own replies to non-subscribed users", async () => {
-      expect(hasPost(posts, w.posts[0].unrelatedOwn)).toBe(true);
-    });
+        it('should NOT include posts by non-subscribed users even if subscribed users answered them', async () => {
+          expect(hasPost(posts, w.posts[0].ownUnrelatedAnswered)).toBe(false);
+        });
 
-    it("should include replies to unavailable posts", async () => {
-      expect(hasPost(posts, w.posts[0].subUnavailableIndirect)).toBe(true);
-    });
-  });
+        it('should NOT include posts by non-subscribed users even if I answered them', async () => {
+          expect(hasPost(posts, w.posts[0].unrelatedAnsweredByProtagonist)).toBe(false);
+        });
+
+        if (lvl > 0) it("should include my 1st level subscription's replies to non-subscribed users", async () => {
+          expect(hasPost(posts, w.posts[0].unrelatedSub)).toBe(true);
+          expect(hasPost(posts, w.posts[0].ownUnrelatedSub)).toBe(true);
+        });
+        else it("should NOT include my 1st level subscription's replies to non-subscribed users", async () => {
+          expect(hasPost(posts, w.posts[0].unrelatedSub)).toBe(false);
+          expect(hasPost(posts, w.posts[0].ownUnrelatedSub)).toBe(false);
+        });
+
+        it("should include my own replies to non-subscribed users", async () => {
+          expect(hasPost(posts, w.posts[0].unrelatedOwn)).toBe(true);
+        });
+
+        if (lvl > 1) it("should include 2nd level replies to unavailable posts", async () => {
+          expect(hasPost(posts, w.posts[0].subUnavailableIndirect)).toBe(true);
+        });
+        else it("should NOT include 2nd level replies to unavailable posts", async () => {
+          expect(hasPost(posts, w.posts[0].subUnavailableIndirect)).toBe(false);
+        });
+      });  // pre-existing posts
+    });  // for each recursion level
+  });  // Veritum output format
 });
