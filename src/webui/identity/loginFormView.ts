b7e1b2d63ca2c1b9a76a341ddba7bd15f9c74841
Victor Hahn <victor@hahn.mt>
2025-05-18 16:08:22 +0200

    WebUI IdentityController: Support bip39 recovery phrase login

--- src/webui/identity/loginFormView.ts
+++ src/webui/identity/loginFormView.ts
@@ -2,6 +2,8 @@ import { VerityView } from "../verityView";
 import type { IdentityController } from "./identityController";
 
 export class LoginFormView extends VerityView {
+  declare controller: IdentityController;
+
   constructor(
       controller: IdentityController,
       htmlTemplate: HTMLTemplateElement = document.getElementById(
@@ -9,6 +11,24 @@ export class LoginFormView extends VerityView {
 
   ){
     super(controller, htmlTemplate);
+    this.setEventHandlers();
   }
 
+  private setEventHandlers(): void {
+    // Password login form
+    const passwordForm: HTMLFormElement = this.renderedView.querySelector(
+      ".verityLoginPasswordForm");
+    passwordForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.controller.performPasswordLogin(passwordForm);
+    });
+
+    // Bip39 login form
+    const bip39Form: HTMLFormElement = this.renderedView.querySelector(
+      ".verityLoginBip39Form");
+    bip39Form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.controller.performBip39Login(bip39Form);
+    });
+  }
 }
