0e6cb285c9209b20cc32081f22636c61297f8d41
Victor Hahn <victor@hahn.mt>
2025-09-06 13:46:45 +0200

    Github#835 Tests: Amend encrypted Veritum end-to-end test
    to try and find the low-probability intermittent failure this test has been experiencing for ages

--- test/cci/e2e/encryptedVerita.test.ts
+++ test/cci/e2e/encryptedVerita.test.ts
@@ -2,6 +2,8 @@ import type { CubeKey } from "../../../src/core/cube/cube.definitions";
 import type { CubeInfo } from "../../../src/core/cube/cubeInfo";
 import type { cciCube } from "../../../src/cci/cube/cciCube";
 
+import { NetConstants } from "../../../src/core/networking/networkDefinitions";
+
 import { FieldType } from "../../../src/cci/cube/cciCube.definitions";
 import { VerityField } from "../../../src/cci/cube/verityField";
 import { Identity } from "../../../src/cci/identity/identity";
@@ -27,15 +29,22 @@ describe('Transmission of encrypted Verita', () => {
       await net.sender.publishVeritum(
         veritum, { recipients: net.recipient.identity, addAsPost: false });
       const key: CubeKey = veritum.getKeyIfAvailable();
-      expect(key).toBeDefined();
 
-      // Note: Creating the veritumPropagated promise here is a bit late, as
-      //   propagation already started while we awaited publishVeritum().
-      //   To compensate, let's create a competing Promise that will resolve
-      //   if it has already propagated.
-      const veritumWillPropagate: Promise<CubeInfo> = net.fullNode2.cubeStore.expectCube(key);
-      const veritumAlreadyArrived: CubeInfo = await net.fullNode2.cubeStore.getCubeInfo(key);
-      const veritumPropagated = veritumAlreadyArrived ? Promise.resolve() : veritumWillPropagate;
+      // Verify test setup:
+      // - Key should be valid
+      expect(key.length).toBe(NetConstants.CUBE_KEY_SIZE);
+      // - Veritum should consist of a single Cube
+      const chunks: cciCube[] = Array.from(veritum.chunks);
+      expect(chunks).toHaveLength(1);
+      // - Encrypted chunk has an ENCRYPTED but no PAYLOAD field
+      expect(chunks[0].getFirstField(FieldType.ENCRYPTED)).toBeDefined();
+      expect(chunks[0].getFirstField(FieldType.PAYLOAD)).toBeUndefined();
+      // - Veritum should be decryptable by the recipient
+      const testDecryption = Veritum.FromChunks(chunks, {
+        recipientPrivateKey: net.recipient.identity.encryptionPrivateKey,
+      });
+      expect(testDecryption.getFirstField(FieldType.PAYLOAD)).toBeDefined();
+      expect(testDecryption.getFirstField(FieldType.ENCRYPTED)).toBeUndefined();
 
       // Reference Veritum thorugh Identity MUC
       // Note: This is also possible to do automatically through publishVeritum();
@@ -49,7 +58,14 @@ describe('Transmission of encrypted Verita', () => {
       const idPropagated: Promise<CubeInfo> =
         net.fullNode2.cubeStore.expectCube(net.sender.identity.key);
 
-      // give it some time to propagate through the network
+      // Wait for network propagation
+      // Note: Creating the veritumPropagated promise here is a bit late, as
+      //   propagation already started while we awaited publishVeritum().
+      //   To compensate, let's create a competing Promise that will resolve
+      //   if it has already propagated.
+      const veritumWillPropagate: Promise<CubeInfo> = net.fullNode2.cubeStore.expectCube(key);
+      const veritumAlreadyArrived: CubeInfo = await net.fullNode2.cubeStore.getCubeInfo(key);
+      const veritumPropagated = veritumAlreadyArrived ? Promise.resolve() : veritumWillPropagate;
       await Promise.all([veritumPropagated, idPropagated]);
 
       // verify test setup
@@ -70,9 +86,23 @@ describe('Transmission of encrypted Verita', () => {
       const retrieved: Veritum = await net.recipient.getVeritum(key);
       // expect Veritum received
       expect(retrieved).toBeDefined();
-      // expect Veritum decrypted
-      expect(retrieved.getFirstField(FieldType.ENCRYPTED)).not.toBeDefined();
 
+      // debugging intermezzo (see Github#835):
+      // Attempt a manual decryption first.
+      // Below, we will still expect the Veritum to have been auto-decrypted
+      // on the initial retrieval.
+      const manualDecryptionCipherchunk: cciCube =
+        await net.recipient.node.cubeStore.getCube(key);
+      const manualDecryptionPlain: Veritum = Veritum.FromChunks(
+        [manualDecryptionCipherchunk],
+        { recipientPrivateKey: net.recipient.identity.encryptionPrivateKey }
+      );
+      expect(manualDecryptionPlain.getFirstField(FieldType.PAYLOAD)).toBeDefined();
+      expect(manualDecryptionPlain.getFirstField(FieldType.PAYLOAD).valueString).toBe(plaintext);
+      expect(manualDecryptionPlain.getFirstField(FieldType.ENCRYPTED)).toBeUndefined();
+
+      // expect Veritum auto-decrypted on initial retrieval
+      expect(retrieved.getFirstField(FieldType.ENCRYPTED)).not.toBeDefined();
       // TODO FIXME this sometimes fails and I don't know why :(
       expect(retrieved.getFirstField(FieldType.PAYLOAD)).toBeDefined();
 
